bf1adaa062a3433e1924a7d119a3991c28729067
PrincessMaggy <edoziemagdalene@gmail.com>
2022-06-03 15:21:59 +0100

    added list functionality

--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 
-const List =() =>{
+const List =({setLists, tweet, lists, key}) =>{
 
+    const deleteTweet = () =>{
+        setLists(lists.filter((state)=> state.id !== tweet.id));
+    }
 return(
-    <div>
-
+    <div className="List">
+        <button> Like</button>
+        <h3>{tweet.task} </h3>
+        <button onClick={deleteTweet}> delete</button>
     </div>
 )
 
