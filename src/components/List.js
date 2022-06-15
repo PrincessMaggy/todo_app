42b901f1a65c0dbda8eabd73bf236b6da8596cdf
PrincessMaggy <edoziemagdalene@gmail.com>
2022-06-15 15:56:34 +0100

    more changes

--- src/components/List.js
+++ src/components/List.js
@@ -6,18 +6,33 @@ const List =({setLists, tweet, lists}) =>{
 
     const checkList =(e)=>{
         e.target.classList.toggle('checked');
+        if (e.target.classList.contains('checked')){
+        e.target.parentElement.nextElementSibling.style.textDecoration= "line-through";
+        }else{
+            e.target.parentElement.nextElementSibling.style.textDecoration= "none";
+        }
     }
-
+    const display =(e)=>{
+        e.target.parentElement.nextElementSibling.style.display = "flex";
+    }
     const deleteTweet = () =>{
         setLists(lists.filter((state)=> state.id !== tweet.id));
     }
+    const cancel =(e)=>{
+        e.target.parentElement.parentElement.style.display = "none";
+    }
+    
 return(
     <div>
         <ul className="list">
             <li>
-                <img src={check} alt="check" onClick={checkList}/>
+                <div><img src={check} alt="check" onClick={checkList}/></div>
                 <h3>{tweet.task} </h3>
-                <img src={bin} alt="bin" onClick={deleteTweet} />
+                <div><img src={bin} alt="bin" onClick={display} /></div>
+                <div className="confirm">
+                    <div><button onClick={deleteTweet}>CONFIRM</button> </div>
+                   <div> <button className="cancel" onClick={cancel}>CANCEL</button></div>
+                </div>
             </li>
         </ul>
     </div>
